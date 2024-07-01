2e61483bd48a5d69dc42cc431afcde7dfd3c1572
Nathan Marques de Aguiar Brandão <nathanmarques20@yahoo.com.br>
2024-07-01 15:50:58 -0300

    feat: assests project page

--- assets/links.js
+++ assets/links.js
@@ -1,12 +1,23 @@
 import imgGithub from "./images/github.svg";
 import imgLinkedin from "./images/linkedin.svg";
 import imgCurriculum from "./images/file.svg";
-import curriculum from "./curriculo.pdf";
+import curriculum from "./Curriculo_Nathan.pdf";
 import imgTechs from "./images/techs.svg";
-import nft from './images/nft.png';
-import pokedex from './images/pokedex.png';
+import sgc from './images/sgc.png';
 import britaldo from './images/britaldo.png';
-import habits from './images/habits.png';
+import movie_match from './images/movie-match.png';
+import face_recognition from './images/face-recognition.png';
+import frHome from './images/face-recognition/home.png';
+import frSignup from './images/face-recognition/signup.png';
+import frSignupSuccess from './images/face-recognition/signup-success.png';
+import frLogin from './images/face-recognition/login.png';
+import frLogged from './images/face-recognition/logged.png';
+import mmHome from './images/movie-match/home.png';
+import mmSignup from './images/movie-match/signup.png';
+import mmLogin from './images/movie-match/login.png';
+import mmModal from './images/movie-match/modal.png';
+import mmAssessments from './images/movie-match/assessments.png';
+import mmList from './images/movie-match/list.png';
 
 export const links = {
   github: {
@@ -24,20 +35,51 @@ export const links = {
   techs: imgTechs,
   techsImages: [
     {
-      url: nft,
-      title: 'Rocket NFT',
+      url: sgc,
+      title: 'Prolinx - SGC',
+    },
+    {
+      url: movie_match,
+      title: 'Movie Match',
     },
     {
       url: britaldo,
       title: 'Britaldo Estoque',
     },
     {
-      url: pokedex,
-      title: 'Pokedex',
+      url: face_recognition,
+      title: 'Sistema de Reconhecimento Facial',
+    }
+  ],
+  projectsImages: [
+    {
+      title: 'Prolinx - SGC',
+      links: []
     },
     {
-      url: habits,
-      title: 'Habits',
+      title: 'Movie Match',
+      links: [
+        mmLogin,
+        mmSignup,
+        mmModal,
+        mmHome,
+        mmAssessments,
+        mmList
+      ]
+    },
+    {
+      title: 'Britaldo Estoque',
+      links: []
+    },
+    {
+      title: 'Sistema de Reconhecimento Facial',
+      links: [
+        frHome,
+        frSignup,
+        frSignupSuccess,
+        frLogin,
+        frLogged
+      ]
     }
   ]
 }
