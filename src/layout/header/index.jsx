3719f05dc2cb05abb0884bd924dbbb84762df820
NathanMarques2001 <nathanmarques20@yahoo.com.br>
2024-07-13 23:49:09 -0300

    fix: responsividade

--- src/layout/header/index.jsx
+++ src/layout/header/index.jsx
@@ -33,10 +33,31 @@ function FullStackSection({ stack }) {
   return <p id="header-fullstack">{stack}</p>;
 }
 
-function AboutMeSection({ experience }) {
+function calculateAge(birthdate) {
+  const today = new Date();
+  const birthDate = new Date(birthdate);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
+function calculateExperience(startYear) {
+  const today = new Date();
+  return today.getFullYear() - startYear;
+}
+
+function AboutMeSection() {
+  const age = calculateAge("2001-11-11");
+  const experience = calculateExperience(2022);
+
   return (
     <div id="header-div-about-me">
-      <p id="header-about-me">{experience}</p>
+      <p id="header-about-me">
+        TENHO {age} ANOS SOU DESENVOLVEDOR FULL STACK COM POUCO MAIS DE {experience} ANOS DE EXPERIÊNCIA TRABALHANDO COMO FREELANCER E ATUANDO NA ÁREA.
+      </p>
     </div>
   );
 }
@@ -46,7 +67,7 @@ export function Header(props) {
     <header>
       <NameIconsSection {...props} />
       <FullStackSection {...props} />
-      <AboutMeSection {...props} />
+      <AboutMeSection />
     </header>
   );
 }
