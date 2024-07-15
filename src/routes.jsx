1e1f81870941e6530138c426603449d98d99fed4
NathanMarques2001 <nathanmarques20@yahoo.com.br>
2024-07-14 23:57:11 -0300

    fix: routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,11 +6,11 @@ export function Rotas() {
   return (
     <Router>
       <Routes>
-        <Route path="/projetos" element={<Home />} />
-        <Route path="/projetos/csapp" element={<ProjectDescription projectTitle="csapp" />} />
-        <Route path="/projetos/movie-match" element={<ProjectDescription projectTitle="movie-match" />} />
-        <Route path="/projetos/britaldo-estoque" element={<ProjectDescription projectTitle="britaldo-estoque" />} />
-        <Route path="/projetos/sistema-reconhecimento-facial" element={<ProjectDescription projectTitle="sistema-reconhecimento-facial" />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/csapp" element={<ProjectDescription projectTitle="csapp" />} />
+        <Route path="/movie-match" element={<ProjectDescription projectTitle="movie-match" />} />
+        <Route path="/britaldo-estoque" element={<ProjectDescription projectTitle="britaldo-estoque" />} />
+        <Route path="/sistema-reconhecimento-facial" element={<ProjectDescription projectTitle="sistema-reconhecimento-facial" />} />
       </Routes>
     </Router>
   );
