4a59a28092cab762dc830ee6ee7144ccb54ff67e
NathanMarques2001 <nathanmarques20@yahoo.com.br>
2024-07-28 00:24:35 -0300

    feat: font-size mobile

--- src/pages/project-description/index.jsx
+++ src/pages/project-description/index.jsx
@@ -11,34 +11,34 @@ export function ProjectDescription({ projectTitle }) {
     <div id='projectDescriptionContainer'>
       <div id='projectDescriptionHeader'>
         <div id='projectDescriptionHeaderText'>
-          <h1 id='projectDescriptionHeaderTextTitle'>{project.title}</h1>
-          <p>{project.description}</p>
+          <h1 id='projectDescriptionHeaderTextTitle' className='projectDescriptionTitle'>{project.title}</h1>
+          <p className='projectDescriptionTextDescription'>{project.description}</p>
         </div>
         <img src={links.projectsImages[projectTitle]?.logo} alt={`Logo ${text.projects[projectTitle]?.title}`} id='projectDescriptionBanner' />
       </div>
       <div className='projectDescriptionDiv'>
-        <h2>TELAS</h2>
+        <h2 className='projectDescriptionTitle'>TELAS</h2>
         <p id='projectDescriptionTextDescriptionScreens' className='projectDescriptionTextDescription'>{project.screens}</p>
         <Carousel images={images} />
       </div>
       <div className='projectDescriptionDiv'>
-        <h2>TECNOLOGIAS</h2>
+        <h2 className='projectDescriptionTitle'>TECNOLOGIAS</h2>
         <p className='projectDescriptionTextDescription'>{project.techs}</p>
       </div>
       <div className='projectDescriptionDiv'>
-        <h2>INFRAESTRUTURA</h2>
+        <h2 className='projectDescriptionTitle'>INFRAESTRUTURA</h2>
         <p className='projectDescriptionTextDescription'>{project.infrastructure}</p>
       </div>
       <div className='projectDescriptionDiv'>
-        <h2>FRONT-END</h2>
+        <h2 className='projectDescriptionTitle'>FRONT-END</h2>
         <p className='projectDescriptionTextDescription'>{project.frontend}</p>
       </div>
       <div className='projectDescriptionDiv'>
-        <h2>BACK-END</h2>
+        <h2 className='projectDescriptionTitle'>BACK-END</h2>
         <p className='projectDescriptionTextDescription'>{project.backend}</p>
       </div>
       <div className='projectDescriptionDiv'>
-        <h2>MAIS INFORMAÇÕES</h2>
+        <h2 className='projectDescriptionTitle'>MAIS INFORMAÇÕES</h2>
         <p className='projectDescriptionTextDescription'>O código-fonte deste projeto está disponível no GitHub. O repositório inclui toda a estrutura do projeto, com documentação simplificada para facilitar a contribuição e manutenção.</p>
         <p className='projectDescriptionTextDescription'>Para mais informações, entre em contato pelo e-mail nathanbrandao1@gmail.com.</p>
       </div>
