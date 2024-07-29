1db03360d63e0f2a34b3f37cc8c13299541d6ab0
NathanMarques2001 <nathanmarques20@yahoo.com.br>
2024-07-29 01:26:45 -0300

    'deploy'

---
+++ deploy.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM Adiciona todas as mudanças ao índice
+git add .
+
+REM Faz o commit das mudanças
+git commit -m "new changes"
+
+REM Empurra as mudanças para a branch main
+git push
+
+
+
+npm run build 
+
+git subtree push --prefix dist origin gh-pages
